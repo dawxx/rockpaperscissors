1fee3bd1d927e325ea4b9a41a602a865f04b34dd
Jakub <jakub.swit@op.pl>
2023-11-10 14:04:10 +0100

    better_print

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def user_choice():
     while True:
         try:
             ui_number = int(input("1: Rock, 2: Paper, 3: Scissors -- "))
+            print()
             if ui_number > 3 or ui_number < 1:
                 print("Podaj liczbe od 1 do 3")
                 continue
@@ -36,17 +37,16 @@ def game():
     ui_choice = user_choice()
     you_win = determine_winner(ui_choice, bot_choice)
 
+    print(f"Your choice: {ui_choice}")
+    print(f"Bot choice: {bot_choice}")
     if you_win:
-        print(f"Your choice: {ui_choice}, Bot choice {bot_choice}")
-        print("You win! :) +1 point for You!")
+        print("*** YOU WIN! ***")
         winner = "User"
     elif you_win is None:
-        print(f"Your choice: {ui_choice}, Bot choice {bot_choice}")
-        print("It's a tie - 0 points")
+        print("*** TIE ***")
         winner = None
     else:
-        print(f"Your choice: {ui_choice}, Bot choice {bot_choice}")
-        print("Bot wins! :( +1 point for Bot!")
+        print("*** BOT WINS! ***")
         winner = "Bot"
     
     return winner
@@ -63,8 +63,8 @@ def main():
         print(f"Round {round+1}")
         winner = game()
         points.append(winner)
-        print("Score:")
-        print(f"User: {points.count('User')} points, Bot {points.count('Bot')} points")
+        print(f"Score:", end=" ")
+        print(f"User: {points.count('User')} --- Bot {points.count('Bot')}")
         print()
     if points.count('User') > points.count('Bot'):
         print("The winner of the game is YOU!!!")
